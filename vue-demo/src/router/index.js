bdd3dda0f524b4330a855e2ac84a3bf0c0446dcb
wen-hong <aaaaqqqqzzzzsss@gmail.com>
2026-01-06 15:19:40 +0800

    20260106upd1

--- vue-demo/src/router/index.js
+++ vue-demo/src/router/index.js
@@ -1,21 +1,22 @@
+// import { createRouter, createWebHistory } from 'vue-router'
+// import { useAuthStore } from '../stores/auth'
+
+// // import Home from '@/views/Home.vue'
+// // import HomeView from '../views/HomeView.vue'
+
+// import Loginbackground from '@/views/Loginbackground.vue'
+// import Login from '@/views/Login.vue'
+// import Register from '@/views/Register.vue'
+// import Resetpassword from '@/views/Resetpassword.vue'
+// import Forgotpassword from '@/views/Forgotpassword.vue'
+// import Forgotpasswordreset from '@/views/Forgotpasswordreset.vue'
+
+// import Dashboard from '@/views/Dashboard.vue'
+// import Updprofiles from '@/views/Updprofiles.vue'
+// import Calendarevent from '@/views/Calendarevent.vue'
+// import Calendardashboard from '@/views/Calendardashboard.vue'
+// // import Calculator from '@/views/Calculator.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import { useAuthStore } from '../stores/auth'
-
-// import Home from '@/views/Home.vue'
-// import HomeView from '../views/HomeView.vue'
-
-import Loginbackground from '@/views/Loginbackground.vue'
-import Login from '@/views/Login.vue'
-import Register from '@/views/Register.vue'
-import Resetpassword from '@/views/Resetpassword.vue'
-import Forgotpassword from '@/views/Forgotpassword.vue'
-import Forgotpasswordreset from '@/views/Forgotpasswordreset.vue'
-
-import Dashboard from '@/views/Dashboard.vue'
-import Updprofiles from '@/views/Updprofiles.vue'
-import Calendarevent from '@/views/Calendarevent.vue'
-import Calendardashboard from '@/views/Calendardashboard.vue'
-// import Calculator from '@/views/Calculator.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -29,57 +30,67 @@ const router = createRouter({
         {
           path: 'login',
           name: 'Login',
-          component: Login,
+          // component: Login,
+          component: () => import('@/views/Login.vue'),
         },
         {
           path: 'register',
           name: 'Register',
-          component: Register,
+          // component: Register,
+          component: () => import('@/views/Register.vue'),
         },
         {
           path: 'forgotpassword',
           name: 'Forgotpassword',
-          component: Forgotpassword,
+          // component: Forgotpassword,
+          component: () => import('@/views/Forgotpassword.vue'),
         },
         {
           path: 'forgotpasswordreset',
           name: 'Forgotpasswordreset',
-          component: Forgotpasswordreset,
+          // component: Forgotpasswordreset,
+          component: () => import('@/views/Forgotpasswordreset.vue'),
         },
       ],
     },
     {
       path: '/inloginbackground',
       name: 'inLoginbackground',
-      component: Loginbackground,
+      // component: Loginbackground,
+      component: () => import('@/views/Loginbackground.vue'),
       meta: { requiresAuth: true },
       children: [
         {
           path: 'resetpassword',
           name: 'Resetpassword',
-          component: Resetpassword,
+          // component: Resetpassword,
+          component: () => import('@/views/Resetpassword.vue'),
         },
       ],
     },
     {
       path: '/dashboard',
-      component: Dashboard,
+      // component: Dashboard,
+      component: () => import('@/views/Dashboard.vue'),
       meta: { requiresAuth: true },
       children: [
         {
           path: 'updprofiles',
           name: 'Updprofiles',
-          component: Updprofiles,
+          // component: Updprofiles,
+          component: () => import('@/views/Updprofiles.vue'),
         },
         {
           path: 'calendarevent',
           name: 'Calendarevent',
-          component: Calendarevent,
+          // component: Calendarevent,
+          component: () => import('@/views/Calendarevent.vue'),
         },
         {
           path: 'calendardashboard',
           name: 'Calendardashboard',
-          component: Calendardashboard,
+          // component: Calendardashboard,
+          component: () => import('@/views/Calendardashboard.vue'),
         },
         // {
         //   path: 'calculator',
